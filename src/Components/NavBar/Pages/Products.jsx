b41f5552a745a5b10271dceaf4a5db51de424f11
william hill <williamhill0831@Hotmail.com>
2023-11-27 11:28:40 -0500

    added items to product page and updated data

--- src/Components/NavBar/Pages/Products.jsx
+++ src/Components/NavBar/Pages/Products.jsx
@@ -1,27 +1,34 @@
 import { images } from '../../Data/Data';
 import styles from '../../Styles/Products.module.css';
 
-const Products = () => {
+const ProductCard = ({ image }) => {
+  const { src, alt, width, height, price } = image;
+
   return (
-    <div>
-      <h2>Shop </h2>
-      {images.map((image) => {
-        console.log(image);
-        let img = image.src;
-        let name = image.alt;
-        let width = image.width;
-        let height = image.height;
-        return (
-          <div className={styles.cards}>
-            <div className={styles.card}>
-              <img src={img} alt={name} width={width} height={height} />
-              <h2>{name}</h2>
-            </div>
-          </div>
-        );
-      })}
+    <div className={styles.card}>
+      <img src={src} alt={alt} width={width} height={height} />
+      <h3>{alt}</h3>
+      <p>{price}</p>
     </div>
   );
 };
 
+const ProductList = ({ images }) => {
+  return (
+    <div className={styles.cards}>
+      {images.map((image, index) => (
+        <ProductCard key={index} image={image} />
+      ))}
+    </div>
+  );
+};
+
+const Products = () => {
+  return (
+    <div>
+      <h2>Shop</h2>
+      <ProductList images={images} />
+    </div>
+  );
+};
 export default Products;
